faa4004d916e07aa7806455e133d08d5971d9342
Yatin <yatin@peerbits.com>
2020-02-12 16:49:07 +0530

    first

---
+++ pages-about-us-about-us-module-es2015.js
@@ -0,0 +1,103 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["pages-about-us-about-us-module"],{
+
+/***/ "./node_modules/raw-loader/dist/cjs.js!./src/app/pages/about-us/about-us.component.html":
+/*!**********************************************************************************************!*\
+  !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/pages/about-us/about-us.component.html ***!
+  \**********************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("<div class=\"container\">\n    <div class=\"row\">\n      <div class=\"col-lg-12 text-center\">\n        <h2 class=\"section-heading text-uppercase\">About</h2>\n        <h3 class=\"section-subheading text-muted\">Lorem ipsum dolor sit amet consectetur.</h3>\n      </div>\n    </div>\n    <div class=\"row\">\n      <div class=\"col-lg-12\">\n        <ul class=\"timeline\">\n          <li>\n            <div class=\"timeline-image\">\n              <img class=\"rounded-circle img-fluid\" src=\"img/about/1.jpg\" alt=\"\">\n            </div>\n            <div class=\"timeline-panel\">\n              <div class=\"timeline-heading\">\n                <h4>2009-2011</h4>\n                <h4 class=\"subheading\">Our Humble Beginnings</h4>\n              </div>\n              <div class=\"timeline-body\">\n                <p class=\"text-muted\">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>\n              </div>\n            </div>\n          </li>\n          <li class=\"timeline-inverted\">\n            <div class=\"timeline-image\">\n              <img class=\"rounded-circle img-fluid\" src=\"img/about/2.jpg\" alt=\"\">\n            </div>\n            <div class=\"timeline-panel\">\n              <div class=\"timeline-heading\">\n                <h4>March 2011</h4>\n                <h4 class=\"subheading\">An Agency is Born</h4>\n              </div>\n              <div class=\"timeline-body\">\n                <p class=\"text-muted\">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>\n              </div>\n            </div>\n          </li>\n          <li>\n            <div class=\"timeline-image\">\n              <img class=\"rounded-circle img-fluid\" src=\"img/about/3.jpg\" alt=\"\">\n            </div>\n            <div class=\"timeline-panel\">\n              <div class=\"timeline-heading\">\n                <h4>December 2012</h4>\n                <h4 class=\"subheading\">Transition to Full Service</h4>\n              </div>\n              <div class=\"timeline-body\">\n                <p class=\"text-muted\">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>\n              </div>\n            </div>\n          </li>\n          <li class=\"timeline-inverted\">\n            <div class=\"timeline-image\">\n              <img class=\"rounded-circle img-fluid\" src=\"img/about/4.jpg\" alt=\"\">\n            </div>\n            <div class=\"timeline-panel\">\n              <div class=\"timeline-heading\">\n                <h4>July 2014</h4>\n                <h4 class=\"subheading\">Phase Two Expansion</h4>\n              </div>\n              <div class=\"timeline-body\">\n                <p class=\"text-muted\">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>\n              </div>\n            </div>\n          </li>\n          <li class=\"timeline-inverted\">\n            <div class=\"timeline-image\">\n              <h4>Be Part\n                <br>Of Our\n                <br>Story!</h4>\n            </div>\n          </li>\n        </ul>\n      </div>\n    </div>\n  </div>");
+
+/***/ }),
+
+/***/ "./src/app/pages/about-us/about-us.component.css":
+/*!*******************************************************!*\
+  !*** ./src/app/pages/about-us/about-us.component.css ***!
+  \*******************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL3BhZ2VzL2Fib3V0LXVzL2Fib3V0LXVzLmNvbXBvbmVudC5jc3MifQ== */");
+
+/***/ }),
+
+/***/ "./src/app/pages/about-us/about-us.component.ts":
+/*!******************************************************!*\
+  !*** ./src/app/pages/about-us/about-us.component.ts ***!
+  \******************************************************/
+/*! exports provided: AboutUsComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AboutUsComponent", function() { return AboutUsComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+
+
+let AboutUsComponent = class AboutUsComponent {
+    constructor() { }
+    ngOnInit() {
+    }
+};
+AboutUsComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+        selector: 'app-about-us',
+        template: tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"](__webpack_require__(/*! raw-loader!./about-us.component.html */ "./node_modules/raw-loader/dist/cjs.js!./src/app/pages/about-us/about-us.component.html")).default,
+        styles: [tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"](__webpack_require__(/*! ./about-us.component.css */ "./src/app/pages/about-us/about-us.component.css")).default]
+    })
+], AboutUsComponent);
+
+
+
+/***/ }),
+
+/***/ "./src/app/pages/about-us/about-us.module.ts":
+/*!***************************************************!*\
+  !*** ./src/app/pages/about-us/about-us.module.ts ***!
+  \***************************************************/
+/*! exports provided: AboutUsModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AboutUsModule", function() { return AboutUsModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm2015/common.js");
+/* harmony import */ var _about_us_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./about-us.component */ "./src/app/pages/about-us/about-us.component.ts");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm2015/router.js");
+
+
+
+
+
+const routes = [
+    {
+        path: '',
+        component: _about_us_component__WEBPACK_IMPORTED_MODULE_3__["AboutUsComponent"]
+    }
+];
+let AboutUsModule = class AboutUsModule {
+};
+AboutUsModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        declarations: [_about_us_component__WEBPACK_IMPORTED_MODULE_3__["AboutUsComponent"]],
+        imports: [
+            _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+            _angular_router__WEBPACK_IMPORTED_MODULE_4__["RouterModule"].forChild(routes)
+        ]
+    })
+], AboutUsModule);
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=pages-about-us-about-us-module-es2015.js.map
